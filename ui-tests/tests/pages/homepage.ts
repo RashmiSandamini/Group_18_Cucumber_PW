fb03b08ef1589b024ec68285fe97ffb54f2e7918
hasathcharu <hasathcharu@users.noreply.github.com>
2024-12-26 12:14:32 +0000

    deploy: a65f339d898fc8fbd36da9a64b1692e72b218f38

--- ui-tests/tests/pages/homepage.ts
+++
@@ -1,9 +0,0 @@
-import BasePage from './basepage';
-import * as locs from '../locators/homepage.json';
-import { Page } from 'playwright';
-
-export default class LoginPage extends BasePage {
-  constructor(page: Page) {
-    super(page, locs.path);
-  }
-}
